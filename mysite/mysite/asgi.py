87f1896fcc13a05ea76059f48b61d37794095900
Ayo Bankole <bankoleayodele147@gmail.com>
2023-04-15 01:07:47 +0100

    deploying the website

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_asgi_application()
